937007e22122c5f9e43d9bc9f9a8b71502c3ce83
Prince Kumar <pg028422@gmail.com>
2026-06-06 19:32:35 +0600

    Solution of  palindrome check for linked list (LeetCode)
    Implement a solution to check if a linked list is a palindrome by finding the middle, reversing the second half, and comparing both halves.

---
+++ Linked List/pallindromCheck.cpp
@@ -0,0 +1,60 @@
+// Problem: Pallindrom Linked List
+// Platform: LeetCode
+// Link: https:https://leetcode.com/problems/palindrome-linked-list/description/
+// Difficulty: Easy
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    ListNode* getMid(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast !=NULL && fast->next !=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow;
+        
+}
+ListNode* reverse(ListNode* head){
+    ListNode* curr= head;
+    ListNode* prev= NULL;
+    ListNode* next=NULL;
+    while(curr !=NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+
+    }
+    return prev;
+
+}
+    bool isPalindrome(ListNode* head) {
+       if(head==NULL || head->next==NULL ){
+        return true;
+    }
+    // step1-> find middle
+    ListNode* middle= getMid(head);
+    // step2-> reverse list after middle
+    ListNode* temp=middle->next;
+    middle->next= reverse(temp);
+
+    // step3- Compare both halves
+    ListNode* head1= head;
+    ListNode* head2= middle->next;
+    while(head2 !=  NULL){
+        if(head1 ->val != head2->val){
+            return false;
+        }
+        head1=head1->next;
+        head2= head2->next;
+    }
+    // Step4 - repeat step 2
+    temp=middle->next;
+    middle->next= reverse(temp);
+
+    return true;
+    }
+};
