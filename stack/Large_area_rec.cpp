b4c513d1dfcb0b8cc48ecd1bc1bf4f0cbe72834e
Prince Kumar <pg028422@gmail.com>
2026-06-15 20:04:10 +0600

    Solution to find the largest area in histrogram

---
+++ stack/Large_area_rec.cpp
@@ -0,0 +1,60 @@
+Link: https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+// Problem: Largest Rectengular Area in Histrigram
+// Platform: LeetCode
+// Difficulty: Hard
+// Approach: Iterative
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+    public:
+    vector<int>nextSmallerElement(vector<int>&arr,int n){
+    stack<int>s;
+    s.push(-1);
+    vector<int>ans(n);
+    for(int i= n-1;i>=0;i--){
+        int curr=arr[i];
+        while(s.top() !=-1 && arr[s.top()]>=curr){
+            s.pop();
+        }
+        ans[i]=s.top();
+        s.push(i);
+    }
+    return ans;
+}
+
+vector<int>prevSmallerElement(vector<int>&arr,int n){
+    stack<int>s;
+    s.push(-1);
+    vector<int>ans(n);
+    for(int i=0;i<n;i++){
+        int curr=arr[i];
+        while(s.top() !=-1 && arr[s.top()]>=curr){
+            s.pop();
+        }
+        ans[i]=s.top();
+        s.push(i);
+    }
+    return ans;
+}
+
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+    vector<int>next(n);
+    vector<int>prev(n);
+    next=nextSmallerElement(heights,n);
+    prev=prevSmallerElement(heights,n);
+    int area=INT8_MIN;
+    for(int i=0; i<n; i++){
+        int l=heights[i];
+        
+        if(next[i]==-1){
+            next[i]=n;
+        }
+        int b=next[i]-prev[i]-1;
+        int newArea=l*b;
+        area=max(area,newArea);
+    }
+    return area;
+    }
+};
