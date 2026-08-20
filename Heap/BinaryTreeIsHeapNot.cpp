ed44d01fc6247a9d8d48ed5bec55704870683e21
Prince Kumar <pg028422@gmail.com>
2026-08-21 01:35:17 +0530

    Add function to check that Binary Tree is Max Heap or Not
    Implement functions to check if a binary tree is a heap.

---
+++ Heap/BinaryTreeIsHeapNot.cpp
@@ -0,0 +1,56 @@
+// Problem: Check that Binary tree is heap or not 
+// Platform: GeeksforGeeks
+// Difficulty: Easy
+// Approach:Recursively
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+int CountNode(Node* root){
+    // base case
+    if(root==NULL){
+        return 0;
+    }
+    int ans=1+CountNode(root->left)+CountNode(root->right);
+    return ans;
+}
+
+bool isCBT(Node* root, int index, int cnt){
+    if(root==NULL){
+        return true;
+    }
+    if(index>=cnt){
+        return false;
+    }
+    else{
+        bool left=isCBT(root->left,2*index+1,cnt);
+        bool right=isCBT(root->right,2*index+2,cnt);
+        return (left && right);
+    }
+}
+
+bool isMaxOrder(Node* root){
+    // leaf node
+    if(root->left==NULL && root->right==NULL){
+        return true;
+    }
+    if(root->right==NULL){
+        return (root->data>root->left->data);
+    }
+    else{
+        bool left=isMaxOrder(root->left);
+        bool right=isMaxOrder(root->right);
+        if(left && right && (root->data>root->left->data && root->data>root->right->data)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+bool isHeap(Node* root){
+    int index=0;
+    int totalCount=CountNode(root);
+    if(isCBT(root,index,totalCount)&&(isMaxOrder)){
+        return true;
+    }
+}
